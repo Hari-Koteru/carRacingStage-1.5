66480983749574969ec3c06390a730977377633f
Hari-Koteru <sudheerreddy.koteru@gmail.com>
2021-01-06 20:47:06 +0530

    game camera

--- sketch.js
+++ sketch.js
@@ -8,9 +8,12 @@ var database;
 
 var form, player, game;
 
+var car1, car2, car3, car4;
 
-function setup(){
-  canvas = createCanvas(400,400);
+var cars; 
+
+function setup() {
+  canvas = createCanvas(displayWidth - 20, displayHeight - 30);
   database = firebase.database();
   game = new Game();
   game.getState();
@@ -18,11 +21,11 @@ function setup(){
 }
 
 
-function draw(){
-  if(playerCount === 4){
+function draw() {
+  if (playerCount === 4) {
     game.update(1);
   }
-  if(gameState === 1){
+  if (gameState === 1) {
     clear();
     game.play();
   }
